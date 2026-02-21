f7cedbb956a8b1d2bf2d0afa36c48dd4b0593759
piyush0001 <piyushsengar881@gmail.com>
2026-02-21 15:14:40 +0530

    read FSR analog value and print ADC output

--- src/main.cpp
+++ src/main.cpp
@@ -37,9 +37,12 @@ void loop() {
 
     // TODO 5:
     // Read analog value from FSR
+    fsrValue = analogRead(FSR_PIN);
 
     // TODO 6:
     // Print raw ADC value
+    Serial.print("FSR Value: ");
+    Serial.println(fsrValue);
 
     // TODO 7:
     // Apply simple threshold logic (e.g., detect pressure)
