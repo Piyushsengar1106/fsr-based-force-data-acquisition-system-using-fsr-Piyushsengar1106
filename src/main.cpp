f38a4d09d767ae96df58be683d41c60b142f3776
piyush0001 <piyushsengar881@gmail.com>
2026-02-21 15:13:18 +0530

    initialize serial communication and print startup message

--- src/main.cpp
+++ src/main.cpp
@@ -22,10 +22,16 @@ void setup() {
 
     // TODO 3:
     // Initialize Serial communication (9600 baud rate)
+    Serial.begin(9600);
+
 
     // TODO 4:
     // Print system initialization message
+    Serial.println("===============================");
+    Serial.println(" FSR Force Measurement System ");
+    Serial.println("===============================");
 }
+
 
 void loop() {
 
